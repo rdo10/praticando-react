c214f355026dc4761dbc2c391bd77f1133d11243
Ricardo Camargo <66854225+rdo10@users.noreply.github.com>
2021-03-01 14:28:12 -0500

    Update list.jsx

--- practica/src/components/list.jsx
+++ practica/src/components/list.jsx
@@ -3,13 +3,15 @@ import { Fragment} from 'react';
 
 const Listado = () => {
     const numbers = [1, 2, 3, 4, 5, 6, 7];
+    const sports = ['soccer','Basket','tennis','baseball']
 
     return (
         <Fragment>
+            <h2>Deportes Favoritos</h2>
             <ul>
                 {
-                    numbers.map((number, index) =>
-                        <li key={index}>{number}</li>
+                    sports.map((sport, index) =>
+                        <li key={index}>{sport}</li>
                     )
                 }
             </ul>
