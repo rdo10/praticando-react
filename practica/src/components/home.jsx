949dcc952711305b7ad9d18426d36e1f6783f15f
Ricardo Camargo <66854225+rdo10@users.noreply.github.com>
2021-03-01 09:34:28 -0500

    añadiendo variables
    mostrar  el valor de las variables en las vistas

--- practica/src/components/home.jsx
+++ practica/src/components/home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 function main(){
+    let saludo = "Hola usuario bienvenido";
     return (
         <div>
          <h1>pagina principal</h1>
+         <p>{saludo}</p>
         </div>
     )
 }
